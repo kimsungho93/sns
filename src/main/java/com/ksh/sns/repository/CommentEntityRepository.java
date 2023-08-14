8d4fc673ba0690a7260acc19417cde922f8d05d2
kimsungho <tigertjdgh@gmail.com>
2023-08-14 23:54:58 +0900

    feat: 포스트 삭제시 관련 좋아요, 댓글 삭제 로직 추가

--- src/main/java/com/ksh/sns/repository/CommentEntityRepository.java
+++ src/main/java/com/ksh/sns/repository/CommentEntityRepository.java
@@ -5,9 +5,19 @@ import com.ksh.sns.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Integer> {
     Page<CommentEntity> findAllByPost(PostEntity post, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE CommentEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity);
 }
--- src/main/java/com/ksh/sns/repository/LikeEntityRepository.java
+++ src/main/java/com/ksh/sns/repository/LikeEntityRepository.java
@@ -4,10 +4,12 @@ import com.ksh.sns.entity.LikeEntity;
 import com.ksh.sns.entity.PostEntity;
 import com.ksh.sns.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,5 +21,10 @@ public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer>
 //    Integer countByPost(@Param("post") PostEntity post);
 
     long countByPost(PostEntity post);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE LikeEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity);
 
 }
